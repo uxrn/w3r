175cdd8d6ad023c9dbd7cbe957e913fa32929d1f
Michael <20736150+uimc@users.noreply.github.com>
2025-02-28 00:54:43 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function searchReg() {
-    var sbInput = document.getElementById('sb-input').value;
-    var sbTLD = sbInput.split('.').pop();
-    var pCount = sbInput.replace(/[^.]/g, "").length;
+function searchReg(input) {
+    var sbTLD = input.split('.').pop();
+    var pCount = input.replace(/[^.]/g, "").length;
     var regURL = "https://porkbun.com/checkout/search?q=";
 
-    if (sbInput == "") {
+    if (input == "") {
         alert("Nothing entered in the search bar.");
         return;
     }
-    else if (sbInput.length > 63) {
+    else if (input.length > 63) {
         alert("Name cannot be longer than 63 characters");
         return;
     }
@@ -16,19 +15,19 @@ function searchReg() {
         alert("Only one period allowed. Do not include 'www.' in your search.");
         return;
     }
-    else if (sbInput.slice(0,1) == '.') {
+    else if (input.slice(0,1) == '.') {
         alert("Name cannot start with a period.");
     }
     else if (pCount == 1 && !(sbTLD == "dn" || sbTLD == "ur" || sbTLD == "rd")) {
         alert("Unsupported TLD. Try searching for a name ending in .dn, .ur, or .rd.");
         return;
     }
-    else if (pCount == 0 && !(sbTLD == "dn" || sbTLD == "ur" || sbTLD == "rd") || (sbInput == "dn" || sbInput == "ur" || sbInput == "rd")) {
-        window.open(regURL + sbInput + ".dn", '_blank').focus();
+    else if (pCount == 0 && !(sbTLD == "dn" || sbTLD == "ur" || sbTLD == "rd") || (input == "dn" || input == "ur" || input == "rd")) {
+        window.open(regURL + input + ".dn", '_blank').focus();
         return;
     }
     else {
-        window.open(regURL + sbInput, '_blank').focus();
+        window.open(regURL + input, '_blank').focus();
         return;
     }
 }
